5700ff47024963c220fb4e40805fa431e6996170
Maksim <maksim.momcilovic@stud.altekanti.ch>
2023-04-11 22:55:15 +0200

    jpeg to bitmap implementation

--- pic_flip/src-tauri/src/main.rs
+++ pic_flip/src-tauri/src/main.rs
@@ -3,24 +3,24 @@
 
 use std::path::Path;
 use image::{ GenericImage, GenericImageView, ImageBuffer, RgbImage };
-use image_convert::{ ImageResource, PNGConfig, to_png };
+use image_convert::{ ImageResource, PNGConfig, BMPConfig, to_png, to_bmp };
 //TODO! Overview methods --> currently test with jpg to png is located in jpg method instead of png method
 #[tauri::command]
 fn convert_image(path: &str, choice: &str) {
     let p = Path::new(path);
     let extension = p.extension().unwrap().to_str().unwrap();
     match extension {
-        "svg" => to_svg(choice, path),
-        "png" => make_png(choice, path),
-        "jpg" => to_jpg(choice, path),
-        "jpeg" => to_jpg(choice, path),
-        "bmp" => to_bmp(choice, path),
-        "gif" => to_gif(choice, path),
+        "svg" => adjust_svg(choice, path),
+        "png" => adjust_png(choice, path),
+        "jpg" => adjust_jpg(choice, path),
+        "jpeg" => adjust_jpg(choice, path),
+        "bmp" => adjust_bmp(choice, path),
+        "gif" => adjust_gif(choice, path),
         _ => println!("There is no accepted format!"),
     }
 }
 
-fn to_svg(extension: &str, path: &str) {
+fn adjust_svg(extension: &str, path: &str) {
     match extension {
         "png" => png_to_svg(path),
         "jpg" => jpeg_to_svg(path),
@@ -30,7 +30,7 @@ fn to_svg(extension: &str, path: &str) {
     }
 }
 
-fn to_bmp(extension: &str, path: &str) {
+fn adjust_bmp(extension: &str, path: &str) {
     match extension {
         "png" => png_to_bmp(path),
         "jpg" => jpg_to_bmp(path),
@@ -40,7 +40,7 @@ fn to_bmp(extension: &str, path: &str) {
     }
 }
 
-fn to_gif(extension: &str, path: &str) {
+fn adjust_gif(extension: &str, path: &str) {
     match extension {
         "png" => png_to_gif(path),
         "jpg" => jpeg_to_gif(path),
@@ -50,7 +50,7 @@ fn to_gif(extension: &str, path: &str) {
     }
 }
 
-fn make_png(extension: &str, path: &str) {
+fn adjust_png(extension: &str, path: &str) {
     match extension {
         "gif" => gif_to_png(path),
         "jpg" => jpeg_to_png(path),
@@ -60,11 +60,11 @@ fn make_png(extension: &str, path: &str) {
     }
 }
 
-fn to_jpg(extension: &str, path: &str) {
+fn adjust_jpg(extension: &str, path: &str) {
     match extension {
         "gif" => gif_to_jpeg(path),
-        "png" => png_to_jpeg(path),
-        "bmp" => bmp_to_jpeg(path),
+        "png" => jpeg_to_png(path),
+        "bmp" => jpeg_to_bmp(path),
         "svg" => svg_to_jpeg(path),
         _ => println!("There is no accepted format!"),
     }
@@ -91,7 +91,7 @@ fn bmp_to_gif(path: &str) {}
 
 // PNG Converspath: &strions
 fn gif_to_png(path: &str) {}
-fn jpeg_to_png(path: &str) {
+fn png_to_jpeg(path: &str) {
     
 }
 fn svg_to_png(path: &str) {}
@@ -99,7 +99,7 @@ fn bmp_to_png(path: &str) {}
 
 // jpeg Converspath: &strions
 fn gif_to_jpeg(path: &str) {}
-fn png_to_jpeg(path: &str) {
+fn jpeg_to_png(path: &str) {
     let source_image_path = Path::new(path);
 
     let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.png");
@@ -115,7 +115,21 @@ fn png_to_jpeg(path: &str) {
     to_png(&mut output, &input, &config).unwrap();
 }
 fn svg_to_jpeg(path: &str) {}
-fn bmp_to_jpeg(path: &str) {}
+fn jpeg_to_bmp(path: &str) {
+    let source_image_path = Path::new(path);
+
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.bmp");
+
+    let mut config = BMPConfig::new();
+
+    config.width = 800;
+
+    let input = ImageResource::from_path(source_image_path);
+
+    let mut output = ImageResource::from_path(target_image_path);
+
+    to_bmp(&mut output, &input, &config).unwrap();
+}
 
 fn main() {
     tauri::Builder
