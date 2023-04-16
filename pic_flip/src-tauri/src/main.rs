8b3d32d63fd9f665d44ea7dd0f2673db97f6f997
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2023-04-16 07:01:28 +0200

    .

--- pic_flip/src-tauri/src/main.rs
+++ pic_flip/src-tauri/src/main.rs
@@ -3,133 +3,115 @@
 
 use std::path::Path;
 use image::{ GenericImage, GenericImageView, ImageBuffer, RgbImage };
-use image_convert::{ ImageResource, PNGConfig, BMPConfig, to_png, to_bmp };
+use image_convert::{ ImageResource, PNGConfig, BMPConfig, GIFConfig, JPGConfig, TIFFConfig, WEBPConfig, ICOConfig, GrayRawConfig, PGMConfig, to_png, to_bmp, to_gif, to_jpg, to_tiff, to_webp, to_ico, to_gray_raw, to_pgm };
 //TODO! Overview methods --> currently test with jpg to png is located in jpg method instead of png method
 #[tauri::command]
 fn convert_image(path: &str, choice: &str) {
-    let p = Path::new(path);
-    let extension = p.extension().unwrap().to_str().unwrap();
-    match extension {
-        "svg" => adjust_svg(choice, path),
-        "png" => adjust_png(choice, path),
-        "jpg" => adjust_jpg(choice, path),
-        "jpeg" => adjust_jpg(choice, path),
-        "bmp" => adjust_bmp(choice, path),
-        "gif" => adjust_gif(choice, path),
+    match choice {
+        "png" => create_png(path),
+        "jpg" => create_jpg(path),
+        "jpeg" => create_jpg(path),
+        "bmp" => create_bmp(path),
+        "gif" => create_gif(path),
+        "tiff" => create_tiff(path),
+        "webp" => create_webp(path),
+        "ico" => create_ico(path),
+        "raw" => create_gray_raw(path),
+        "pgm" => create_pgm(path),
         _ => println!("There is no accepted format!"),
-    }
+    };
 }
 
-fn adjust_svg(extension: &str, path: &str) {
-    match extension {
-        "png" => png_to_svg(path),
-        "jpg" => jpeg_to_svg(path),
-        "bmp" => bmp_to_svg(path),
-        "gif" => gif_to_svg(path),
-        _ => println!("There is no accepted format!"),
-    }
-}
-
-fn adjust_bmp(extension: &str, path: &str) {
-    match extension {
-        "png" => png_to_bmp(path),
-        "jpg" => jpg_to_bmp(path),
-        "svg" => svg_to_bmp(path),
-        "gif" => gif_to_bmp(path),
-        _ => println!("There is no accepted format!"),
-    }
-}
-
-fn adjust_gif(extension: &str, path: &str) {
-    match extension {
-        "png" => png_to_gif(path),
-        "jpg" => jpeg_to_gif(path),
-        "bmp" => bmp_to_gif(path),
-        "svg" => svg_to_gif(path),
-        _ => println!("There is no accepted format!"),
-    }
-}
-
-fn adjust_png(extension: &str, path: &str) {
-    match extension {
-        "gif" => gif_to_png(path),
-        "jpg" => jpeg_to_png(path),
-        "bmp" => bmp_to_png(path),
-        "svg" => svg_to_png(path),
-        _ => println!("There is no accepted format!"),
-    }
-}
-
-fn adjust_jpg(extension: &str, path: &str) {
-    match extension {
-        "gif" => gif_to_jpeg(path),
-        "png" => jpeg_to_png(path),
-        "bmp" => jpeg_to_bmp(path),
-        "svg" => svg_to_jpeg(path),
-        _ => println!("There is no accepted format!"),
-    }
-}
-
-// SVG Conversions
-fn png_to_svg(path: &str) {}
-
-fn jpeg_to_svg(path: &str) {}
-fn bmp_to_svg(path: &str) {}
-fn gif_to_svg(path: &str) {}
-
-// Bitmap Converspath: &strions
-fn png_to_bmp(path: &str) {}
-fn jpg_to_bmp(path: &str) {}
-fn svg_to_bmp(path: &str) {}
-fn gif_to_bmp(path: &str) {}
-
-// GIF Converspath: &strions
-fn png_to_gif(path: &str) {}
-fn jpeg_to_gif(path: &str) {}
-fn svg_to_gif(path: &str) {}
-fn bmp_to_gif(path: &str) {}
-
-// PNG Converspath: &strions
-fn gif_to_png(path: &str) {}
-fn png_to_jpeg(path: &str) {
-    
-}
-fn svg_to_png(path: &str) {}
-fn bmp_to_png(path: &str) {}
-
-// jpeg Converspath: &strions
-fn gif_to_jpeg(path: &str) {}
-fn jpeg_to_png(path: &str) {
+fn create_pgm(path: &str) {
     let source_image_path = Path::new(path);
-
-    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.png");
-
-    let mut config = PNGConfig::new();
-
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.pgm");
+    let mut config = PGMConfig::new();
     config.width = 800;
-
     let input = ImageResource::from_path(source_image_path);
-
     let mut output = ImageResource::from_path(target_image_path);
+    to_pgm(&mut output, &input, &config).unwrap();
+}
 
+fn create_gray_raw(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.raw");
+    let mut config = GrayRawConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
+    to_gray_raw(&mut output, &input, &config).unwrap();
+}
+
+fn create_jpg(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.jpg");
+    let mut config = JPGConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
+    to_jpg(&mut output, &input, &config).unwrap();
+}
+
+fn create_ico(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.ico");
+    let mut config = ICOConfig::new();
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
+    to_ico(&mut output, &input, &config).unwrap();
+}
+
+fn create_webp(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.webp");
+    let mut config = WEBPConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
+    to_webp(&mut output, &input, &config).unwrap();
+}
+
+fn create_png(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.png");
+    let mut config = PNGConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
     to_png(&mut output, &input, &config).unwrap();
 }
-fn svg_to_jpeg(path: &str) {}
-fn jpeg_to_bmp(path: &str) {
+
+fn create_gif(path: &str){
     let source_image_path = Path::new(path);
-
-    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.bmp");
-
-    let mut config = BMPConfig::new();
-
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.gif");
+    let mut config = GIFConfig::new();
     config.width = 800;
-
     let input = ImageResource::from_path(source_image_path);
-
     let mut output = ImageResource::from_path(target_image_path);
+    to_gif(&mut output, &input, &config).unwrap();
+}
 
+fn create_bmp(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.bmp");
+    let mut config = BMPConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
     to_bmp(&mut output, &input, &config).unwrap();
 }
+
+fn create_tiff(path: &str){
+    let source_image_path = Path::new(path);
+    let target_image_path = Path::join(source_image_path.parent().unwrap(), "new.tiff");
+    let mut config = TIFFConfig::new();
+    config.width = 800;
+    let input = ImageResource::from_path(source_image_path);
+    let mut output = ImageResource::from_path(target_image_path);
+    to_tiff(&mut output, &input, &config).unwrap();
+}
+
+
 
 fn main() {
     tauri::Builder
